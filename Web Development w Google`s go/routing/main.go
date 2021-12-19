669b2c3e76946e6a826e8ab917a39378adab1bc6
Мазеин Николай Иванович <n.mazein@2gis.ru>
2021-12-19 21:35:36 +0700

    use julienschmidt router

--- Web Development w Google`s go/routing/main.go
+++ Web Development w Google`s go/routing/main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
-	"io"
+	"github.com/julienschmidt/httprouter"
+	"html/template"
+	"log"
 	"net/http"
 )
 
-func d(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "dog dog doggy")
-}
+var tpl *template.Template
 
-func c(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "cat cat kitty")
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
 func main() {
+	mux := httprouter.New()
+	mux.GET("/", index)
+	mux.GET("/apply", apply)
+	mux.POST("/apply", applyProcess)
+	http.ListenAndServe(":8080", mux)
+}
 
-	// using default handler
-	http.HandleFunc("/dog/", d)
-	http.HandleFunc("/cat", c)
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "index.html", nil)
+	HandleError(w, err)
+}
 
-	http.ListenAndServe(":8080", nil)
+func apply(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "apply.html", nil)
+	HandleError(w, err)
+}
+
+func applyProcess(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "applyProcess.html", nil)
+	HandleError(w, err)
+}
+
+func HandleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
 }
