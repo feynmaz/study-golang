ead108622f41f024bd9a75e8280a1dbc0c189e21
Мазеин Николай Иванович <n.mazein@2gis.ru>
2021-12-19 20:45:52 +0700

    nested templates

--- Web Development w Google`s go/templates/main.go
+++ Web Development w Google`s go/templates/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"math"
 	"os"
 	"text/template"
 )
@@ -10,29 +9,11 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.New("index.gohtml").Funcs(fm).ParseFiles("index.gohtml"))
-}
-
-func double(x int) int {
-	return x + x
-}
-
-func square(x int) float64 {
-	return math.Pow(float64(x), 2)
-}
-
-func sqrt(x float64) float64 {
-	return math.Sqrt(x)
-}
-
-var fm = template.FuncMap{
-	"fdbl":  double,
-	"fsq":   square,
-	"fsqrt": sqrt,
+	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
 func main() {
-	err := tpl.Execute(os.Stdout, 3)
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.html", 4)
 	if err != nil {
 		log.Fatalln(err)
 	}
