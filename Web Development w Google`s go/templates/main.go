5083ad6edae7f833d10bda868798cb829b2b276f
Мазеин Николай Иванович <n.mazein@2gis.ru>
2021-12-19 20:14:32 +0700

    pass complex data to template

--- Web Development w Google`s go/templates/main.go
+++ Web Development w Google`s go/templates/main.go
@@ -8,13 +8,64 @@ import (
 
 var tpl *template.Template
 
+type sage struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+type items struct {
+	Wisdom    []sage
+	Transport []car
+}
+
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
 }
 
 func main() {
-	data := []string{"Hello", "World"}
-	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", data)
+
+	buddha := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+
+	gandhi := sage{
+		"Gandhi",
+		"Be the change",
+	}
+
+	jesus := sage{
+		"Jesus",
+		"Love all",
+	}
+
+	f := car{
+		"Ford",
+		"F150",
+		2,
+	}
+
+	c := car{
+		"Toyota",
+		"Corolla",
+		4,
+	}
+
+	sages := []sage{buddha, gandhi, jesus}
+	cars := []car{f, c}
+
+	data := items{
+		sages,
+		cars,
+	}
+
+	err := tpl.Execute(os.Stdout, data)
 	if err != nil {
 		log.Fatalln(err)
 	}
