9a603d83ec4ceff33b9755ddad9918e5dd104e61
Мазеин Николай Иванович <n.mazein@2gis.ru>
2021-12-19 21:23:57 +0700

    routing with mux

--- Web Development w Google`s go/routing/main.go
+++ Web Development w Google`s go/routing/main.go
@@ -8,15 +8,26 @@ import (
 type hotdog int
 
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/dog":
-		io.WriteString(w, "doggy doggy doggy")
-	case "/cat":
-		io.WriteString(w, "kitty kitty kitty")
-	}
+	io.WriteString(w, "dog dog doggy")
+}
+
+type hotcat int
+
+func (c hotcat) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "cat cat kitty")
 }
 
 func main() {
 	var d hotdog
-	http.ListenAndServe(":8080", d)
+	var c hotcat
+
+	mux := http.NewServeMux()
+
+	// /dog/anything/else
+	mux.Handle("/dog/", d)
+
+	// /cat
+	mux.Handle("/cat", c)
+
+	http.ListenAndServe(":8080", mux)
 }
