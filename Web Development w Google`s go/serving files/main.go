ccbcb74a47d18cadf8426c3902ad8e51ef445525
Мазеин Николай Иванович <n.mazein@2gis.ru>
2021-12-28 03:13:37 +0700

    Dir

--- Web Development w Google`s go/serving files/main.go
+++ Web Development w Google`s go/serving files/main.go
@@ -6,16 +6,12 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", dog)
-	http.HandleFunc("/dog.jpg", dogPic)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/dog", dog)
 	http.ListenAndServe(":8080", nil)
 }
 
 func dog(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	io.WriteString(w, `<img src="/dog.jpg">`)
-}
-
-func dogPic(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "dog.jpg")
 }
