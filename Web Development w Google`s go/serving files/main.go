6fe001559a251fff70e8ea54fbd20314e595fe1a
feynmaz <feynmaz@gmail.com>
2022-01-04 00:51:04 +0500

    static server

--- Web Development w Google`s go/serving files/main.go
+++ Web Development w Google`s go/serving files/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"io"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", dog)
-	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
-	http.ListenAndServe(":8080", nil)
-}
-
-func dog(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	io.WriteString(w, `<img src="/resources/dog.jpg">`)
+	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
 }
