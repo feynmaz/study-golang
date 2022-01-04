a2c294abb178b242197850ebe1d3064fa464e3fc
feynmaz <feynmaz@gmail.com>
2022-01-04 17:36:36 +0500

    hands-on 1

--- Web Development w Google`s go/serving files/main.go
+++ Web Development w Google`s go/serving files/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 )
 
+
+var tpl *template.Template
+
+func init () {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/dog/", dog)
+
+	http.Handle("/media/", http.StripPrefix("/media", http.FileServer(http.Dir("media/"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "foo ran")
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "dog.html", nil)
 }
