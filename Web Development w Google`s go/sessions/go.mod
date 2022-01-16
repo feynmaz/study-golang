e03c6732b143e854f19b01f2c3ba35b637238eeb
feynmaz <feynmaz@gmail.com>
2022-01-16 05:29:50 +0500

    session use case

--- Web Development w Google`s go/sessions/go.mod
+++ Web Development w Google`s go/sessions/go.mod
@@ -4,4 +4,7 @@ go 1.17
 
 require github.com/satori/go.uuid v1.2.0
 
-require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+require (
+	github.com/gorilla/schema v1.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- Web Development w Google`s go/sessions/main.go
+++ Web Development w Google`s go/sessions/main.go
@@ -2,30 +2,83 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"github.com/satori/go.uuid"
+	"html/template"
+	"github.com/gorilla/schema"
 )
+
+var decoder = schema.NewDecoder()
+
+type user struct {
+	UserName string
+	FirstName string
+	LastName string
+}
+
+var tpl *template.Template
+var dbUsers = make(map[string]user) // user ID - user
+var dbSessions = make(map[string]string) // session ID - user ID
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
 
 func main() {
 	http.HandleFunc("/favicon.ico", faviconHandler)
 	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
 
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("session")
+	c, err := r.Cookie("session")
 	if err != nil {
-		id := uuid.NewV4()
-		cookie = &http.Cookie{
-			Name: "session-id",
-			Value: id.String(),
-			Secure: true, // https only
-			HttpOnly: true, // cannot be accesed by js
+		sID := uuid.NewV4()
+		c = &http.Cookie{
+			Name: "session",
+			Value: sID.String(),
 		}
-		http.SetCookie(w, cookie)
+		http.SetCookie(w, c)
 	}
-	fmt.Println(cookie)
+
+	var u user
+	if userName, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[userName]
+	}
+	
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalf("Cannot parse form: %v", err)
+		}
+		err = decoder.Decode(&u, r.Form)
+		if err != nil {
+			log.Fatalf("Cannot decode form: %v", err)
+		}
+		dbSessions[c.Value] = u.UserName
+		dbUsers[u.UserName] = u
+	}
+	tpl.ExecuteTemplate(w, "index.html", u)
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("session")
+	if err != nil {
+		fmt.Println(err)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	} 
+	un, ok := dbSessions[c.Value]
+	if !ok {
+		fmt.Println("not ok")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+	u := dbUsers[un]
+	tpl.ExecuteTemplate(w, "bar.html", u)
 }
 
 func faviconHandler(w http.ResponseWriter, r *http.Request) {
