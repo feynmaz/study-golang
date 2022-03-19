7f8be0e8b16af5174454ee49f68febaf8720120e
feynmaz <feynmaz@gmail.com>
2022-03-19 17:03:15 +0500

    methods

--- Learn How To Code/functions/main.go
+++ Learn How To Code/functions/main.go
@@ -2,17 +2,32 @@ package main
 
 import "fmt"
 
+type person struct {
+	first string
+	last  string
+	age   uint8
+}
+
+type agent struct {
+	person
+	ltk bool
+}
+
+func (a agent) speak() {
+	fmt.Println(a.last)
+	fmt.Println(a.first, a.last)
+}
+
 func main() {
-	defer foo()
-	bar()
-}
+	a := agent{
+		person: person{
+			"James",
+			"Bond",
+			35,
+		},
+		ltk: true,
+	}
 
-func foo() {
-	fmt.Println("foo")
+	fmt.Println(a)
+	a.speak()
 }
-
-func bar() {
-	fmt.Println("bar")
-}
-
-// func (r receiver) identifier(parameters) (return(s)) { ... }
