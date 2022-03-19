44da43fe356d7e4999432387490d00b775682d19
feynmaz <feynmaz@gmail.com>
2022-03-20 00:40:20 +0500

    returning a func

--- Learn How To Code/functions/main.go
+++ Learn How To Code/functions/main.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	s1 := foo()
+	fmt.Println(s1)
 
-	f := func(x int){
-		fmt.Printf("Anonymous func run, printing %v\n", x)
+	s2 := bar()
+	fmt.Printf("%T\n", s2)
+	fmt.Println(s2())
+}
+
+func foo() string {
+	s := "Hello, world!"
+	return s
+}
+
+func bar() func() int {
+	return func() int {
+		return 451
 	}
-
-	f(4)
-
-
 }
