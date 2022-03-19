fa341e9b98e105ec46a5838ec7829f4f3f1f7e05
feynmaz <feynmaz@gmail.com>
2022-03-19 13:30:04 +0500

    anonymous struct

--- Learn How To Code/grouping data/main.go
+++ Learn How To Code/grouping data/main.go
@@ -2,32 +2,18 @@ package main
 
 import "fmt"
 
-// struct is data structure which allows us to compose togeether values of different types
-type person struct {
-	first string
-	last string
-}
-
-type secretAgent struct {
-	person
-	ltk bool
-}
 
 func main() {
-	// value of type person
-	p1 := person{
-		first: "James",
-		last: "Bond",
+	// anonymous struct
+	p1 := struct{
+		first string
+		last string
+		age int
+	}{
+		"James",
+		"Bond",
+		32,
 	}
 
-	fmt.Println(p1.last)
 	fmt.Println(p1)
-
-	a1 := secretAgent{
-		person: p1,
-		ltk: true,
-	}
-
-	fmt.Println(a1)
-
 }
